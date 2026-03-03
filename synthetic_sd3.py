369633a5569c5784a2dee8667e763da100bcfa3c
James Baker <jlbaker361@gmail.com>
2026-03-03 12:35:18 -0500

    synth

---
+++ synthetic_sd3.py
@@ -0,0 +1,55 @@
+import os
+from accelerate import Accelerator
+from datasets import Dataset
+
+subject_path="subjects.txt"
+style_path="styles.txt"
+
+with open(subject_path,"r") as subject_file:
+    subject_list=subject_file.readlines()
+    
+with open(style_path, "r") as style_file:
+    style_list=style_file.readlines()
+    
+accelerator=Accelerator()
+device=accelerator.device
+
+print(device)
+
+src_dict={
+    "image":[],
+    "subject":[],
+    "style":[]
+}
+
+import torch
+from diffusers import StableDiffusion3Pipeline
+import time
+
+pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-medium", torch_dtype=torch.bfloat16)
+pipe = pipe.to(device)
+
+start=time.time()
+limit=30
+
+for b,sub in enumerate(subject_list):
+    for y,sty in enumerate(style_list):
+        
+        if b*100 +y ==limit:
+            end=time.time()
+            print(f"total time = {end-start}, aka {(end-start)/(b*100 +y)}")
+        elif b+100+y >limit:
+            break
+        else:
+
+            image = pipe(
+                f"{sub} {sty}",
+                num_inference_steps=40,
+                guidance_scale=4.5,
+            ).images[0]
+            
+            src_dict["image"].append(image)
+            src_dict["style"].append(sty)
+            src_dict["subject"].append(sub)
+
+Dataset.from_dict(src_dict).push_to_hub("jlbaker361/synthetic-sd3")
