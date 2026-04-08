ad02408dba2e7333575a341ac666b52de89e5dcf
James Baker <jlbaker361@gmail.com>
2026-04-08 02:44:22 -0400

    made new

---
+++ sae_autopsy.py
@@ -0,0 +1,14 @@
+from overcomplete.sae import TopKSAE,SAE,RATopKSAE, RAJumpSAE,QSAE,BatchTopKSAE,MpSAE
+
+for model in [TopKSAE,SAE,RATopKSAE, RAJumpSAE,QSAE,BatchTopKSAE,MpSAE]:
+    input_shape=10
+    nb_concepts=10_000
+    
+    sae:SAE=model(input_shape=input_shape,nb_concepts=nb_concepts)
+    
+    for name,module in sae.named_children():
+        print(name, type(module),end="")
+        if hasattr(module,"weight"):
+            print(module.weight.cpu().detach().size())
+        else:
+            print(" ")
