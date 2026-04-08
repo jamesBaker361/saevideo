a3a50d3a73a59397dc8dfbac63e19f707555e52f
James Baker <jlbaker361@gmail.com>
2026-04-08 02:45:08 -0400

    h

--- sae_autopsy.py
+++ sae_autopsy.py
@@ -5,6 +5,7 @@ for model in [TopKSAE,SAE,RATopKSAE, RAJumpSAE,QSAE,BatchTopKSAE,MpSAE]:
     nb_concepts=10_000
     
     sae:SAE=model(input_shape=input_shape,nb_concepts=nb_concepts)
+    print(model,type(sae), model.__name__)
     
     for name,module in sae.named_children():
         print(name, type(module),end="")
