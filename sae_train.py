a48b0f44a191181f4ee92a771f879965a72a47eb
James Baker <jlbaker361@gmail.com>
2026-04-19 22:48:04 -0400

    training better?

--- sae_train.py
+++ sae_train.py
@@ -34,10 +34,9 @@ QUANTIZED="quantized"
 parser.add_argument("--nb_concepts",type=int,default=10000,help="n concepts for SAE")
 parser.add_argument("--sae_model",type=str,default=KSAE)
 parser.add_argument("--model_layer",type=str,default="up_blocks.1.attentions.0")
-parser.add_argument("--local_global_split",action="store_true",help="if yes, split the concepts into global and local components; global need to be the same across time and/or location")
-parser.add_argument("--src_dir_list",nargs="*",default=["seg_ip_flickr"])
-parser.add_argument("--dino",action="store_true",help="whether to use dino embeddings too")
-parser.add_argument("--mask",action="store_true",help="whether to mask out irrelevant tensors")
+parser.add_argument("--src_dir_list",nargs="*",default=["seg_ip_laion"])
+parser.add_argument("--use_dino",action="store_true",help="whether to use dino embeddings too")
+parser.add_argument("--use_mask",action="store_true",help="whether to mask out irrelevant tensors")
 parser.add_argument("--flatten",action="store_true",help="whether to do the whole image at once or do channel by channel")
 parser.add_argument("--timestep",type=int,default=2,help="which timestep to use (probably doesn't matter)")
 parser.add_argument("--hf_data",action="store_true")
@@ -95,79 +94,49 @@ def get_num(path):
     return int(nums[-1])  # last number in filename
     
 class LatentLocalDataset(torch.utils.data.Dataset):
-    def __init__(self,src_dir_list:Optional[str|list[str]],checkpoint:str,step:int,model_layer:str,dino:bool,
-                 use_mask:bool,limit:int,flatten:bool,pooling:str,threshold:float,
-                 device):
-        self.model_layer=model_layer
-        self.src_dir_list=src_dir_list
+    def __init__(self,src_dir_list:Optional[str|list[str]],
+                 model_layer:str,
+                 use_mask:bool):
+        
         super().__init__()
         if type(src_dir_list)==str:
             src_dir_list=[src_dir_list]
-        self.np_list=[]
-        self.dino_list=[]
-        self.mask_list=[]
-        shape_dict=get_shape_dict(checkpoint,device)
-        (b,c,h,w)=shape_dict[model_layer]
-        self.h=h
-        self.w=w
-        self.c=c
-        self.dino=dino
-        self.mask=use_mask
+            
+        self.model_layer=model_layer
+        self.src_dir_list=src_dir_list
+        self.use_mask=use_mask
+
+        self.np_list = []
         for src_dir in src_dir_list:
             self.np_list+=sorted([
-                os.path.join(src_dir,str(step),f) for f in os.listdir(os.path.join(src_dir,str(step))) if f.endswith("npz")
+                os.path.join(src_dir,f) for f in os.listdir(src_dir) if f.endswith("npz")
             ],key=get_num)
-
-            if dino:
-                self.dino_list+=sorted([
-                    os.path.join(src_dir,"dino",f) for f in os.listdir(os.path.join(src_dir,"dino")) if f.endswith("npy")
-                ],key=get_num)
-            if use_mask:
-                self.mask_list+=sorted([
-                    os.path.join(src_dir,"mask",f) for f in os.listdir(os.path.join(src_dir,"mask")) if f.endswith("npy")
-                ],key=get_num)
-        self.pool={
-            "max":F.max_pool2d,
-            "avg":F.avg_pool2d
-        }[pooling]
-        if use_mask:
-            mask=torch.tensor(np.load(self.mask_list[0]))
-            (m_h,m_w)=mask.size()
-            self.kernel=m_h//h
-        self.threshold=threshold
-        self.flatten=flatten
         
 
     def __len__(self):
         return len(self.np_list)
     
     def __getitem__(self, index):
-        ret={"act": torch.tensor(np.load(self.np_list[index])[self.model_layer][0])}
-        (c,h,w)=ret["act"].size()
-        num=get_num(self.np_list[index])
-        if self.mask:
-            mask=np.load(self.mask_list[index])
-            ret["mask"] = torch.tensor(mask).unsqueeze(0)
-            #print("mask path ",self.mask_list[index])
-
-            ret["mask"]=Resize((h,w),)(ret["mask"])
-            ret["mask"] = (ret["mask"] > self.threshold).to(torch.uint8)
-
-            ret["act"]=ret["mask"]*ret["act"]
-        if self.flatten:
-            ret["act"]=ret["act"].flatten()
-        else:
-            ret["act"]=ret["act"].permute(1,2,0).flatten(0,1) # c,h,w -> (h,w,c) -> (hw,c)
-        if self.dino:
-            ret["dino"]=torch.tensor(np.load(self.dino_list[index])[0][0])
-            if not self.flatten:
-                ret["dino"]=ret["dino"].unsqueeze(-1).unsqueeze(-1).expand(-1,h,w) #384 -> 384,1,1 -> 384,h,w
-                if self.mask:
-                    ret["dino"]=ret["dino"]*ret["mask"]
-                ret["dino"]=ret["dino"].permute(1,2,0).flatten(0,1) # 384,h,w -> (384,w,c) -> (hw,384)
-            
-            
-        return ret
+        npz_dict=np.load(self.np_list[index])
+        inputs=npz_dict["cached_input."+self.model_layer]
+        outputs=npz_dict["cached_output."+self.model_layer]
+        diff=outputs-inputs
+        mask=np.sum(npz_dict["mask"],axis=0)
+        
+        print(mask.shape)
+        print(diff.shape)
+        valid = (mask != 0)
+        diff = diff[:, :, valid]
+        print(diff.shape)
+        
+        diff=torch.tensor(diff).permute(0,2,1).flatten(0,1) #b,c,hw -> bhw,c
+        dino=torch.tensor(npz_dict["dino"])
+        
+        return {
+            "act":diff,
+            "dino":dino
+        }
+        
     
 
 def main(args):
@@ -189,8 +158,8 @@ def main(args):
     model_layer : str = args.model_layer
     local_global_split  = args.local_global_split
     src_dir_list  = args.src_dir_list
-    dino  = args.dino
-    mask  = args.mask
+    use_dino  = args.use_dino
+    use_mask  = args.use_mask
     flatten  = args.flatten
     timestep : int = args.timestep
     hf_data  = args.hf_data
@@ -209,14 +178,20 @@ def main(args):
     }[args.sae_model]
     
     criterion={
-        KSAE:losses.mse_l1 #TODO: find losses for other models
-    }[args.sae_model]
+        KSAE:losses.mse_l1,
+        JUMP:losses.mse_l1,
+        BATCHK:losses.mse_l1,
+        QUANTIZED:losses.mse_l1,
+    }.get(args.sae_model)
+    if criterion is None:
+        raise NotImplementedError(f"No loss defined for sae_model={args.sae_model}")
     
     istopk=(sae_model==KSAE)
 
-    dataset= LatentLocalDataset(args.src_dir_list,args.checkpoint,
-                                args.step,args.model_layer,args.dino,args.mask,args.limit,args.flatten,args.pooling,
-                                args.threshold,device)
+    shape_dict=get_shape_dict(args.checkpoint,device)
+    dataset= LatentLocalDataset(args.src_dir_list,
+                                args.model_layer,
+                                use_mask)
     
     train_loader,test_loader,val_loader=split_data(dataset,0.95,args.batch_size)
     
@@ -225,15 +200,11 @@ def main(args):
     
     print("real activation size ",batch["act"].size())
     
-    shape_dict=get_shape_dict(args.checkpoint,device)
+    
     print(shape_dict[args.model_layer])
     (b,c,h,w)=shape_dict[args.model_layer]
     
-    if args.flatten:
-    
-        sae_model:SAE=sae_model_class(c*h*w,args.nb_concepts,device=device)
-    else:
-        sae_model:SAE=sae_model_class(c,args.nb_concepts,device=device)
+    sae_model:SAE=sae_model_class(c,args.nb_concepts,device=device)
         
     
     params=[p for p in sae_model.parameters()]
@@ -241,20 +212,16 @@ def main(args):
        "sae" :sae_model
     }
     
-    if args.dino:
+    if args.use_dino:
         dino=batch["dino"]
         print("dino size ",dino.size())
-        if args.flatten:
-            (b,dc)=dino.size() #(b,384hw)
-        else:
-            (b,hw,dc)=dino.size() #(b,hw,384)
+        (b,dc)=dino.size() #(b,384hw)
         dino_sae_model:SAE=sae_model_class(dc,args.nb_concepts,device=device)
         params.extend([p for p in dino_sae_model.parameters()])
         model_dict["dino_sae"]=dino_sae_model
         
     act=batch["act"]
-    if not args.flatten:
-        act=act.flatten(0,1) #(b,hw,c) -> (bhw,c)
+    act=act.flatten(0,1) #(b,hw,c) -> (bhw,c)
     with accelerator.autocast():
         z_pre, z, x_hat=sae_model(act.to(device))
     dead_tracker = DeadCodeTracker(z.size()[1], device)
@@ -264,19 +231,16 @@ def main(args):
 
     optimizer=torch.optim.AdamW(params,args.lr)
     
-    if args.dino:
+    if args.use_dino:
         dino_sae_model,optimizer,sae_model,train_loader,test_loader,val_loader = accelerator.prepare(dino_sae_model,optimizer,sae_model,train_loader,test_loader,val_loader)
     else:
         optimizer,sae_model,train_loader,test_loader,val_loader = accelerator.prepare(optimizer,sae_model,train_loader,test_loader,val_loader)
 
-    step=str(args.step)
     save_subdir=os.path.join("sae_model")
     os.makedirs(save_subdir,exist_ok=True)
     save_subdir=os.path.join("sae_model",args.prefix)
     os.makedirs(save_subdir,exist_ok=True)
     save_subdir=os.path.join("sae_model",args.prefix,args.model_layer)
-    os.makedirs(save_subdir,exist_ok=True)
-    save_subdir=os.path.join("sae_model",args.prefix,args.model_layer,step)
     os.makedirs(save_subdir,exist_ok=True)
     
 
@@ -291,7 +255,7 @@ def main(args):
         torch.save(sae_model.state_dict(), weights_path)
 
         # optional DINO weights
-        if args.dino:
+        if args.use_dino:
             dino_weights_path = os.path.join(save_subdir, "dino_weights.pt")
             torch.save(dino_sae_model.state_dict(), dino_weights_path)
 
@@ -311,7 +275,7 @@ def main(args):
             sae_model.load_state_dict(torch.load(weights_path))
 
             # optional DINO
-            if args.dino:
+            if args.use_dino:
                 dino_weights_path = os.path.join(load_dir, "dino_weights.pt")
                 if os.path.exists(dino_weights_path):
                     dino_sae_model.load_state_dict(torch.load(dino_weights_path))
@@ -357,34 +321,41 @@ def main(args):
             
         
         activations=batch["act"].to(device)
-        if not args.flatten:
-            activations=activations.flatten(0,1)
+        activations=activations.flatten(0,1)
             
-        if args.dino:
+        if args.use_dino:
             dino=batch["dino"].to(device)
-            if not args.flatten:
-                dino=dino.flatten(0,1)
+            dino=dino.flatten(0,1)
         if training:
-            optimizer.zero_grad()
+            
             models=[sae_model]
-            if args.dino:
+            if args.use_dino:
                 models.append(dino_sae_model)
             with accelerator.accumulate(*models):
                 with accelerator.autocast():
+                    optimizer.zero_grad()
                     z_pre, z, x_hat=sae_model(activations)
+                    
+                    bhw=z.size()[0]
                     
                     loss=criterion(activations, x_hat, z_pre, z, sae_model.get_dictionary())
                     
-                    if args.dino: 
+                    logging_dict={
+                        "loss":loss.cpu().detach().numpy()
+                    }
+                    
+                    if args.use_dino: 
                         z_pre_dino, z_dino, x_hat_dino=dino_sae_model(dino)
                         
                         dino_loss=criterion(dino,x_hat_dino,z_pre_dino,z_dino,dino_sae_model.get_dictionary())
                         
-                        difference=args.dino_coefficient*F.mse_loss(z_dino,z)
+                        z_dino=z_dino.expand(bhw,nb_concepts)
+                        difference=F.mse_loss(z_dino,z)
+                        logging_dict["dino_loss"]=dino_loss.cpu().detach().numpy()
+                        logging_dict["differences"]=difference.cpu().detach().numpy()
+                        loss+=args.dino_coefficient*difference+dino_loss
                         
-                        loss+=difference+dino_loss
-                        
-                    
+                    accelerator.log(logging_dict)
                     accelerator.backward(loss)
                     optimizer.step()
                     dead_tracker.update(z)
@@ -394,12 +365,12 @@ def main(args):
                     
                 loss=criterion(activations, x_hat, z_pre, z, sae_model.get_dictionary())
                 
-                if args.dino: 
+                if args.use_dino: 
                     z_pre_dino, z_dino, x_hat_dino=dino_sae_model(dino)
                     
                     dino_loss=criterion(dino,x_hat_dino,z_pre_dino,z_dino,dino_sae_model.get_dictionary())
                     
-                    difference=args.dino_coefficient*F.mse_loss(z_dino,z)
+                    difference=args.use_dino_coefficient*F.mse_loss(z_dino,z)
                     
                     loss+=difference+dino_loss
                     
