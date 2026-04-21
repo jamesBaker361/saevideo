5ec6dff99649e8972d2d8f135a6c81e020135b1f
James Baker <jlbaker361@gmail.com>
2026-04-20 23:37:48 -0400

    h

--- boothing.py
+++ boothing.py
@@ -311,6 +311,8 @@ def main(args):
                 
             if torch.isnan(recons).any():
                 print("recons nan")
+            else:
+                print("recons okay :)")
             if use_bias:
                 recons=recons+sae.pre_bias
             recons=recons.unsqueeze(-1).unsqueeze(-1)
@@ -448,25 +450,25 @@ def main(args):
 
 
             with accelerator.accumulate(params):
-                #with accelerator.autocast(): #possibly THIS is bad???
+                with accelerator.autocast(): #possibly THIS is bad???
                     
-                model_pred = unet.forward(
-                    noisy_model_input,timesteps,
-                                        encoder_hidden_states=prompt_embeds,
-                                        timestep_cond=timestep_cond,
-                                        added_cond_kwargs=added_cond_kwargs,
-                                        return_dict=False,
-                )[0]
-                
-                if scheduler.config.prediction_type == "epsilon":
-                    target = noise
-                elif scheduler.config.prediction_type == "v_prediction":
-                    target = scheduler.get_velocity(latents, noise, timesteps)
+                    model_pred = unet.forward(
+                        noisy_model_input,timesteps,
+                                            encoder_hidden_states=prompt_embeds,
+                                            timestep_cond=timestep_cond,
+                                            added_cond_kwargs=added_cond_kwargs,
+                                            return_dict=False,
+                    )[0]
                     
-                loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
-                accelerator.backward(loss)
-                optimizer.step()
-                optimizer.zero_grad()
+                    if scheduler.config.prediction_type == "epsilon":
+                        target = noise
+                    elif scheduler.config.prediction_type == "v_prediction":
+                        target = scheduler.get_velocity(latents, noise, timesteps)
+                        
+                    loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+                    accelerator.backward(loss)
+                    optimizer.step()
+                    optimizer.zero_grad()
             loss_list.append(loss.cpu().detach().numpy())
             
         accelerator.log({
