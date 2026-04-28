8e9dad0d7f43046645cb19b35389016adc05eaa4
James Baker <jlbaker361@gmail.com>
2026-04-27 23:55:48 -0400

    sizing

--- sae_train.py
+++ sae_train.py
@@ -136,6 +136,9 @@ class LatentLocalDataset(torch.utils.data.Dataset):
         
         diff=torch.tensor(diff).permute(0,3,2,1).flatten(0,2) #b,c,h,w -> bhw,c
         dino=torch.tensor(npz_dict["dino"])
+        print(dino.size())
+        print(dino[:,0,:].size())
+        print(dino[:,0,:].view(-1,384).size())
         dino=dino[:,0,:].view(-1,384).expand(diff.size(),-1)
         
         return {
